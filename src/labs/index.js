8108bac00ad306220d2513b12c614bf9121e2724
susan3321 <susan3321>
2022-10-24 10:33:00 -0700

    a6

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,10 +1,13 @@
 import Assignment6 from "./a6";
 import Nav from "../nav";
+import TodoList from "./a6/todo/todo-list";
+
 function Labs() {
     return (
         <div>
             <Nav/>
             <Assignment6/>
+            <TodoList/>
         </div>
     );
 }

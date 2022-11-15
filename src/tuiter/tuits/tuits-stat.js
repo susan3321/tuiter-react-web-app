b6ee9495ac785c2632e9e4772abed3d9dc87bf95
susan3321 <susan3321>
2022-11-14 20:35:45 -0800

    a8

--- src/tuiter/tuits/tuits-stat.js
+++ src/tuiter/tuits/tuits-stat.js
@@ -11,14 +11,19 @@ const TuitsStat = (input) => {
     return (
         <>
                 <div className="row">
-                    <div className="col-3"><i className="bi bi-chat"></i> {tuit.replies}</div>
-                    <div className="col-3"><i className="bi bi-reply"></i> {tuit.retuits}</div>
+                    <div className="col-2"><i className="bi bi-chat"></i> {tuit.replies}</div>
+                    <div className="col-2"><i className="bi bi-reply"></i> {tuit.retuits}</div>
                     <div className="col-3">
                         <i onClick={() => dispatch(updateTuitThunk({
                             ...tuit,
                             likes: tuit.likes + 1
                         }))} className="bi bi-heart-fill me-2 text-danger"></i>{tuit.likes}</div>
-                    <div className="col-3"><i className="bi bi-box-arrow-up"></i></div>
+                    <div className="col-3">
+                        <i onClick={() => dispatch(updateTuitThunk({
+                            ...tuit,
+                            dislikes: tuit.dislikes + 1
+                        }))} className="bi bi-heartbreak me-2 text-dark"></i>{tuit.dislikes}</div>
+                    <div className="col-2"><i className="bi bi-box-arrow-up"></i></div>
 
                 </div>
         </>

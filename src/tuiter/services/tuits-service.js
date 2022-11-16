18dd51670e47227ab0a41e20aa0bb0af18423d30
susan3321 <susan3321>
2022-11-15 17:51:32 -0800

    a9

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // const TUITS_API = `${API_BASE}/tuits`;
-const TUITS_API = 'https://tuiter-node-server-app10.herokuapp.com/api/tuits';
+const TUITS_API = 'https://tuiter-node-server-app10a9.herokuapp.com/api/tuits';
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
